d3e23aa4bf9f0c6c343fc911671510b7663a6313
Kakooza Vianey <35223697+kaksv@users.noreply.github.com>
2024-02-27 23:28:17 +0300

    Update main.rs

--- week1_rust/Day2/owner_func/main.rs
+++ week1_rust/Day2/owner_func/main.rs
@@ -1,1 +1,25 @@
+//Ownership In Functions
+fn main() {
+    let s = String::from("enjoy");
+    takes_ownership(s);
+
+    let t1 =gives_owner("myname".to_string());
+
+    let x =8;
+    copy_me(x);
+
+
+}
+fn takes_ownership(my_string: String) {
+    println!("{}, on.", my_string);
+}
+fn copy_me(num:i32) {
+    println!("{}",num);
+}
+// ->
+fn gives_owner(declared:String) ->String {
+    let string_returned = String::from("here");
+    string_returned
+}
+
 
