effa5fed9c4ef81fef8d06ede9b08ff6e45d7b6f
Kakooza Vianey <35223697+kaksv@users.noreply.github.com>
2024-02-27 23:26:32 +0300

    Update main.rs

--- week1_rust/Day2/express/main.rs
+++ week1_rust/Day2/express/main.rs
@@ -1,1 +1,13 @@
+fn main() {
+    let x = five();
+    println!("x value: {}", x);
+    let y = plus_one(10);
+    println!("Y value: {}", y);
+}
 
+fn five() -> i32 {
+    10
+}
+fn plus_one(my_num: i32) -> i32 {
+    my_num + 1
+}
