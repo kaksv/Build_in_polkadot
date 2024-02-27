0eeb928c5f74e520460305ac26a9f5e13a5f3744
Kakooza Vianey <35223697+kaksv@users.noreply.github.com>
2024-02-27 23:29:49 +0300

    Update main.rs

--- week1_rust/Day2/ownership/main.rs
+++ week1_rust/Day2/ownership/main.rs
@@ -1,1 +1,20 @@
 
+fn main() {
+    
+    //String Type
+    // let mut s =String::from("hello");
+    // s.push_str("decentracode");
+    // print!("{}",s);
+
+    let s1 = String::from("here");
+    let s2 = s1;
+
+    println!("The value of s2 is {}",s2);
+
+    //clone()
+
+    let m1 = String::from("Decent");
+    let m2 = m1;
+    
+    
+}
