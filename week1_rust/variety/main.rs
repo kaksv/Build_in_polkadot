a3cbb33f6142a648543fa899133d7acff31b863f
Kakooza Vianey <35223697+kaksv@users.noreply.github.com>
2024-02-27 05:18:47 +0300

    Update main.rs

--- week1_rust/variety/main.rs
+++ week1_rust/variety/main.rs
@@ -1,1 +1,30 @@
+fn main() {
+    let x:i32 = 5;
+    let y = 'm';
+    let z = "hello";
 
+    {
+        let mut m = 2;
+    }
+
+    let guess: u32 = "42".parse().expect("We expect an integer");
+
+    let p:f64 = 2.0;
+    let q = 3.5;
+    let r = p + q;
+
+    let tup = (3, 4.5, 6, 'i');
+    let t = tup.2;
+    println!("{}",t);
+
+    //hello
+    ///this is a turple structure
+
+    
+    
+    println!("Hello, world!");
+}
+
+fn my_sum (){
+    
+}
